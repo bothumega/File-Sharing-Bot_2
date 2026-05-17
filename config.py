f021ee97c44835810319bcef4f628b07f89ee2e7
bothumega <megahu3333@gmail.com>
2026-05-17 13:36:15 +0530

    Update START_MSG for improved user interaction

--- config.py
+++ config.py
@@ -58,7 +58,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime}"
 
 USER_REPLY_TEXT = "❌Don't Send Me Messages Directly I'm Only File Share Bot !"
 
-START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI Can Store Private Files In Specified Channel And Other Users Can Access It From Special Link.")
+START_MSG = os.environ.get("START_MESSAGE", "Hᴇʟʟᴏ {mention}\n\nI Oɴʟʏ Sᴛᴏʀᴇ Aɴᴅ Sʜᴀʀᴇ @MasterExBackup Fɪʟᴇs Wɪᴛʜ Tʀᴜsᴛᴇᴅ Usᴇʀs.")
 
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need To Join In My Channel/Group To Use Me\n\nKindly Please Join Channel</b>")
 
